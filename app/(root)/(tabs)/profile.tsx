a6abc0917aaa78fd7792f2a696ee0c73fc230b23
Jake Donovan <jakedonovan0227@Jakes-Mac-mini.local>
2025-11-25 15:54:04 -0500

    Added fallback values for profile page

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -63,26 +63,22 @@ const Profile = () => {
         showsVerticalScrollIndicator={false}
         contentContainerClassName="pb-32 px-7"
       >
-        <View className="flex flex-row items-center justify-between mt-5">
-          <Text className="text-xl font-rubik-bold">Profile</Text>
-
-          <Image source={icons.bell} className="size-5" />
-        </View>
-
         <View className="flex flex-row justify-center mt-5">
           <View className="flex flex-col items-center relative mt-5">
-            <Image
-              source={{
-                uri: user?.avatar,
-              }}
-              className="size-44 relative rounded-full"
-            />
-
-            <TouchableOpacity className="absolute bottom-11 right-2">
-              <Image source={icons.edit} className="size-9" />
-            </TouchableOpacity>
-
-            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
+            {user?.avatar ? (
+              <Image
+                source={{ uri: user.avatar }}
+                className="size-44 relative rounded-full"
+              />
+            ) : (
+              <Image
+                source={icons.person}
+                className="size-44 relative rounded-full"
+              />
+            )}
+            <Text className="text-2xl font-rubik-bold mt-2">
+              {user?.name ?? "Guest"}
+            </Text>
           </View>
         </View>
 
