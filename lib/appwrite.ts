f3128ac0044cb71829eaac82d4d7a743d4fb4cc2
Jake Donovan <jakedonovan0227@Jakes-Mac-mini.local>
2025-11-26 12:55:55 -0500

    Created appwrite tables and components for properties and featured properties and hooked up filtering and search input to correctly use seeded data in appwrite database

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,11 +1,29 @@
-import { Client, Avatars, Account, OAuthProvider } from "react-native-appwrite";
+import {
+  Client,
+  Avatars,
+  Account,
+  OAuthProvider,
+  TablesDB,
+  Query,
+} from "react-native-appwrite";
 import { makeRedirectUri } from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
+
+interface GetPropertiesProps {
+  filter?: string;
+  query?: string;
+  limit?: number;
+}
 
 export const config = {
   platform: "com.jd.real-estate-app",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  agentsTableName: "agents",
+  propertiesTableName: "properties",
+  galleriesTableName: "galleries",
+  reviewsTableName: "reviews",
 };
 
 export const client = new Client();
@@ -19,6 +37,8 @@ client
 export const avatar = new Avatars(client);
 // generate new user accounts
 export const account = new Account(client);
+// generate new databases
+export const databases = new TablesDB(client);
 
 export async function login() {
   try {
@@ -116,5 +136,60 @@ export async function getCurrentUser() {
   } catch (error) {
     console.error("Error getting current user: ", error);
     return null;
+  }
+}
+
+export async function getLatestProperties() {
+  try {
+    const result = await databases.listRows({
+      databaseId: config.databaseId!,
+      tableId: config.propertiesTableName!,
+      queries: [Query.orderAsc("$createdAt"), Query.limit(5)],
+    });
+
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getProperties({
+  filter,
+  query,
+  limit,
+}: GetPropertiesProps) {
+  try {
+    const buildQuery = [Query.orderDesc("$createdAt")];
+
+    if (filter && filter !== "All") {
+      buildQuery.push(Query.equal("type", filter));
+    }
+
+    if (query) {
+      buildQuery.push(
+        Query.or([
+          Query.search("name", query),
+          Query.search("address", query),
+          Query.search("description", query),
+          Query.search("type", query),
+        ])
+      );
+    }
+
+    if (limit) {
+      buildQuery.push(Query.limit(limit));
+    }
+
+    const result = await databases.listRows({
+      databaseId: config.databaseId!,
+      tableId: config.propertiesTableName!,
+      queries: buildQuery,
+    });
+
+    return result.rows;
+  } catch (error) {
+    console.error(error);
+    return [];
   }
 }
